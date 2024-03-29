4e180f997622d2be65608f8e900e77ca72b63963
Joanadayse <daysejoana3@gmail.com>
2024-03-29 10:00:56 -0300

    inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 >App Quiz</h1>
+
+<li>ion-item-sliding</li>
+
+<br><br>
+
+<img src="./src/assets/img/tela1.png"/>
+
+<br>
+
+<img src="./src/assets/img/tela2.png"/>
+<br>
+
+<img src="./src/assets/img/tela3.png"/>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
+
+
 import { Component } from '@angular/core';
+import { NavController} from '@ionic/angular';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  alertButtons = ['Action'];
+  public items: Array<any>=[
+    'ideia tem acento?',
+    'Bem vindo tem acento?',
+    'Giboia tem acento?',
+    'Aurelio tem acento?'
+  ];
+
+  constructor(public navCtrl:NavController) {}
+
+  reordenarItens(indexes:any):void{
+    // reorderArray(this.items,indexes)
+    
+      let element = this.items[indexes.from];
+      this.items.splice(indexes.from, 1);
+      this.items.splice(indexes.to, 0, element);
+    
+  }
+
+  btn(id:any):void{
+    alert("Voce respondeu:" + id + "-ACERTOU!")
+  }
+  bt(id:any):void{
+    alert("Voce respondeu:" + id + "-ERROU!")
+  }
+
+ 
+
 
 }
